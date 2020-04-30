16d6dde8fc60131308f4e7b123fb1ed75a2d3590
RUSTAM_PC <ra198167@gmail.com>
2020-04-30 20:21:10 +0400

    Create project

---
+++ src/elements/Footer/index.js
@@ -0,0 +1,12 @@
+import "./style.css";
+import React from 'react'
+
+const Footer = ()=>{
+  return(
+  <footer className="footer">
+    <div className="footer__container">footer</div>
+  </footer>
+  )
+}
+
+export default Footer
---
+++ src/elements/Grid.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Grid = ()=>{
+  return(
+    <div>
+      <div>Grid</div>
+    </div>
+  )
+}
+export default Grid
